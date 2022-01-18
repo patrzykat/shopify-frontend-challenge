a0970e5d9f0cf314f3caa3e05fa935418855ac4e
patrzykat <ilyapatrzykat@gmail.com>
2022-01-17 21:13:15 -0800

    add date selection

--- src/components/chooseDate.tsx
+++ src/components/chooseDate.tsx
@@ -4,18 +4,20 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export default function ChooseDate({ date, setDate }: ChooseDatePropType) {
   return (
-    <div className="flex justify-center mt-10">
-      <div className="flex">
-        <span className="text-sm border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">
-          Date:
-        </span>
-        <DatePicker
-          className="border-2 rounded-r px-4 py-2 w-full"
-          selected={date}
-          onChange={(newDate) => setDate(newDate)}
-          dateFormat="yyyy-MM-dd"
-        />
+    <>
+      <div className="flex justify-center mt-10">
+        <div className="flex flex-col">
+          <DatePicker
+            className="rounded-lg border-2 border-black px-4 py-2 w-full text-center"
+            selected={date}
+            onChange={(newDate) => setDate(newDate)}
+            dateFormat="yyyy-MM-dd"
+          />
+        </div>
       </div>
-    </div>
+      <p className="block text-xs text-center mt-2">
+        Photos from the week of selected date
+      </p>
+    </>
   );
 }
